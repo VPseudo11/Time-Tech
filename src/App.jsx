6849cd56735a9bc53ef39f3c59fc7b100a43f5bb
Pseudo <v.pseudo.11@gmail.com>
2022-07-29 17:12:10 -0500

    add navigator.geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { WEATHER_API_KEY, WEATHER_API_URL } from './api'
 import './App.css'
 import CurrentWeather from './components/CurrentWeather'
@@ -6,36 +6,53 @@ import Forecast from './components/Forecast'
 import Search from './components/Search'
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [forecast, setForecast] = useState(null)
+    const [currentWeather, setCurrentWeather] = useState(null)
+    const [forecast, setForecast] = useState(null)
 
+    useEffect(() => {
+        const succes = ((pos) => {
+            console.log(pos.coords);
+            const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`)
+            const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${WEATHER_API_KEY}&units=metric`)
 
-  const handleOnChangeChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(' ')
+            Promise.all([currentWeatherFetch, forecastFetch])
+                .then(async (response) => {
+                    const weatherResponse = await response[0].json()
+                    const forecastResponse = await response[1].json()
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+                    const nameCountry = `${weatherResponse.name}, ${weatherResponse.sys.country}`
 
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json()
-        const forecastResponse = await response[1].json()
+                    setCurrentWeather({ city: nameCountry, ...weatherResponse })
+                    setForecast({ city: nameCountry.name, ...forecastResponse })
+                })
+                .catch((err) => console.log(err))
+        })
+        navigator.geolocation.getCurrentPosition(succes)
+    }, [])
 
-        setCurrentWeather({ city: searchData.label, ...weatherResponse })
-        setForecast({ city: searchData.label, ...forecastResponse })
-      })
-      .catch((err) => console.log(err))
-  }
+    const handleOnChangeChange = (searchData) => {
+        const [lat, lon] = searchData.value.split(' ')
 
-  console.log(forecast)
+        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+        const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 
-  return (
-    <div className="App">
-      <Search onSearchChange={handleOnChangeChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
-    </div>
-  )
+        Promise.all([currentWeatherFetch, forecastFetch])
+            .then(async (response) => {
+                const weatherResponse = await response[0].json()
+                const forecastResponse = await response[1].json()
+
+                setCurrentWeather({ city: searchData.label, ...weatherResponse })
+                setForecast({ city: searchData.label, ...forecastResponse })
+            })
+            .catch((err) => console.log(err))
+    }
+    return (
+        <div className="App">
+            <Search onSearchChange={handleOnChangeChange} />
+            {currentWeather && <CurrentWeather data={currentWeather} />}
+            {forecast && <Forecast data={forecast} />}
+        </div>
+    )
 }
 
 export default App
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,24 +1,65 @@
 import React from 'react'
-import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from 'react-accessible-accordion'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemPanel,
+    AccordionItemButton
+}
+    from 'react-accessible-accordion'
 import './Forecast.css'
 
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 const Forecast = ({ data }) => {
-    console.log('in forecast' + data.list.splice(0, 7))
+    const dayInAWeek = new Date().getDay()
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek))
+
     return (
         <>
             <label className='title'>Daily</label>
             <Accordion allowZeroExpanded>
                 {data.list.splice(0, 7).map((item, index) => {
-                    <AccordionItem key={index}>
+                    return (<AccordionItem >
                         <AccordionItemHeading>
                             <AccordionItemButton>
                                 <div className='daily-item'>
                                     <img src={`/icons/black/${item.weather[0].icon}.png`} alt="weater" className='iconSmall' />
+                                    <label className='day'>{forecastDays[index]}</label>
+                                    <label className='description'>{item.weather[0].description}</label>
+                                    <label className='min-max'>{item.main.temp_min}°C - {item.main.temp_max}°C</label>
                                 </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
-                        <AccordionItemPanel></AccordionItemPanel>
-                    </AccordionItem>
+                        <AccordionItemPanel>
+                            <div className='daily-details-grid'>
+                                <div className='daily-details-grid-item'>
+                                    <label>Feels Like</label>
+                                    <label>{item.main.feels_like}°C</label>
+                                </div>
+                                <div className='daily-details-grid-item'>
+                                    <label>Pressure</label>
+                                    <label>{item.main.pressure} hPa</label>
+                                </div>
+                                <div className='daily-details-grid-item'>
+                                    <label>Humidity</label>
+                                    <label>{item.main.humidity}%</label>
+                                </div>
+                                <div className='daily-details-grid-item'>
+                                    <label>Clouds</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+                                <div className='daily-details-grid-item'>
+                                    <label>Wind Speed</label>
+                                    <label>{item.wind.speed} m/s</label>
+                                </div>
+                                <div className='daily-details-grid-item'>
+                                    <label>Sea Level</label>
+                                    <label>{item.main.sea_level} m</label>
+                                </div>
+                            </div>
+                        </AccordionItemPanel>
+                    </AccordionItem>)
                 })}
             </Accordion>
         </>
