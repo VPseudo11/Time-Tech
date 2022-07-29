0a1c9e7e95f13022e587afe58f55a23fa96eddad
Pseudo <v.pseudo.11@gmail.com>
2022-07-28 23:06:27 -0500

    TimeTech 60%

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '87661e89d2msh6985ffa58933e8dp10cae5jsnd318012666ec',
+		'X-RapidAPI-Host': 'geo-services-by-mvpc-com.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://geo-services-by-mvpc-com.p.rapidapi.com'
+
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'cf570b82f6b37ea036f59674f874a577'
+
